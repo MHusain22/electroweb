6d8265d1be8e07fb1fcc1804728c3c99f2d08785
MHusain22 <husain.vijapura@gmail.com>
2024-02-03 20:22:12 +0530

    new changes2

--- components/ProductData.js
+++ components/ProductData.js
@@ -26,7 +26,7 @@ export const ProductData = [
     price: "199",
     quantity: "1",
     description:
-      "The iPhone 13 Pro, a pinnacle of innovation and performance, boasts a stunning Super Retina XDR display, powerful A15 Bionic chip, and an advanced triple-camera system. Elevate your mobile experience with ProMotion technology, capturing pro-grade photos and videos with ease. With 5G capability and impressive battery life, the iPhone 13 Pro is your gateway to a world of seamless connectivity and unmatched creativity.",
+      "Sony headphones are renowned for delivering an immersive audio experience with cutting-edge technology and stylish design. Offering an extensive range, Sony headphones boast features such as industry-leading noise-canceling technology, high-resolution audio playback, and customizable sound settings. Whether you're a music enthusiast, a gamer, or a professional seeking top-tier audio quality, Sony headphones consistently deliver exceptional sound clarity and comfort.",
   },
   {
     id: 3,
@@ -37,7 +37,7 @@ export const ProductData = [
     price: "800",
     quantity: "1",
     description:
-      "The iPhone 13 Pro, a pinnacle of innovation and performance, boasts a stunning Super Retina XDR display, powerful A15 Bionic chip, and an advanced triple-camera system. Elevate your mobile experience with ProMotion technology, capturing pro-grade photos and videos with ease. With 5G capability and impressive battery life, the iPhone 13 Pro is your gateway to a world of seamless connectivity and unmatched creativity.",
+      "Nikon cameras are renowned for their exceptional image quality and advanced features, catering to both amateur and professional photographers. Known for their precision optics and cutting-edge technology, Nikon offers a diverse range of cameras, from compact point-and-shoots to powerful DSLRs and mirrorless models. With innovative features such as high-resolution sensors, fast autofocus systems, and robust build quality, Nikon cameras are a popular choice for capturing stunning photos and videos across various genres. ",
   },
   {
     id: 4,
@@ -48,7 +48,7 @@ export const ProductData = [
     price: "300",
     quantity: "1",
     description:
-      "The iPhone 13 Pro, a pinnacle of innovation and performance, boasts a stunning Super Retina XDR display, powerful A15 Bionic chip, and an advanced triple-camera system. Elevate your mobile experience with ProMotion technology, capturing pro-grade photos and videos with ease. With 5G capability and impressive battery life, the iPhone 13 Pro is your gateway to a world of seamless connectivity and unmatched creativity.",
+      "The FireBoult Smart Watch is a cutting-edge wearable device that seamlessly blends style and functionality.From fitness tracking and heart rate monitoring to message notifications and music control, the FireBoult Smart Watch keeps you connected and informed throughout your day. With its durable build and water-resistant design, this smartwatch is a versatile companion for both active lifestyles and daily tasks, providing a convenient and stylish way to stay on top of your health and connected to your digital world.",
   },
   {
     id: 5,
@@ -59,7 +59,7 @@ export const ProductData = [
     price: "56",
     quantity: "1",
     description:
-      "The iPhone 13 Pro, a pinnacle of innovation and performance, boasts a stunning Super Retina XDR display, powerful A15 Bionic chip, and an advanced triple-camera system. Elevate your mobile experience with ProMotion technology, capturing pro-grade photos and videos with ease. With 5G capability and impressive battery life, the iPhone 13 Pro is your gateway to a world of seamless connectivity and unmatched creativity.",
+      "The PS4 gamepad, officially known as the DualShock 4 controller, is the primary input device for Sony's PlayStation 4 gaming console. Featuring a sleek and ergonomic design. The controller includes a touch-sensitive pad, motion-sensing capabilities, and a built-in speaker for immersive gaming experiences. With a variety of buttons, triggers, and analog sticks, the DualShock 4 provides precise control for gaming, while the integrated light bar adds a dynamic element to gameplay and can be used for player identification.",
   },
   {
     id: 6,
@@ -70,6 +70,6 @@ export const ProductData = [
     price: "3999",
     quantity: "1",
     description:
-      "The iPhone 13 Pro, a pinnacle of innovation and performance, boasts a stunning Super Retina XDR display, powerful A15 Bionic chip, and an advanced triple-camera system. Elevate your mobile experience with ProMotion technology, capturing pro-grade photos and videos with ease. With 5G capability and impressive battery life, the iPhone 13 Pro is your gateway to a world of seamless connectivity and unmatched creativity.",
+      "The Apple MacBook is a line of sleek and powerful laptops designed and manufactured by Apple Inc. Known for their premium build quality, high-resolution Retina displays, and efficient hardware, MacBook models offer a seamless integration of hardware and software, running on the macOS operating system. With a focus on portability, performance, and a minimalist design, MacBooks are popular among professionals, creatives, and users seeking a reliable and stylish computing experience.",
   },
 ];
