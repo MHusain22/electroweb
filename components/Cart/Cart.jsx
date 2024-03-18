7112d151ef902c1b073d93597487116dc591293d
MHusain22 <husain.vijapura@gmail.com>
2024-03-18 11:22:45 +0530

    adding payment stripe

--- components/Cart/Cart.jsx
+++ components/Cart/Cart.jsx
@@ -7,37 +7,34 @@ import Navbar from "../header/Navbar";
 import CartTable from "./CartTable";
 import { useDispatch, useSelector } from "react-redux";
 import { cartTotal } from "@/store/slices/CartSlice";
+import { checkout } from "./checkout";
 
 const Cart = () => {
   const dispatch = useDispatch();
-  const { cart,totalPrice, totalQuantity } = useSelector(
+  const { cart, totalPrice, totalQuantity } = useSelector(
     (state) => state.cart
   );
   useEffect(() => {
     dispatch(cartTotal());
-  },[cart])
+  }, [cart]);
 
   return (
     <>
-    <div className={classes.head}>
-      <div
-        className={classes.container}
-        style={{ background: "hsl(0, 0%, 90%)" }}
-      >
-        <Navbar />
-      </div>
+      <div className={classes.head}>
+        <div
+          className={classes.container}
+          style={{ background: "hsl(0, 0%, 90%)" }}
+        >
+          <Navbar />
+        </div>
 
-
-      <div className={classes.row}>
-        
+        <div className={classes.row}>
           <CartTable />
-          
-       
-        <div className={classes.column}>
-         
+
+          <div className={classes.column}>
             <span className={classes.fl}>
               <h3 className={classes.subtotal}>Cart Total</h3>
-              
+
               <span className={classes.price}>{`$${totalPrice}`}</span>
             </span>
             <hr />
@@ -51,16 +48,27 @@ const Cart = () => {
             <hr />
             <span className={classes.rn}>
               <h3 className={classes.subtotal}>Total</h3>
-              <span className={`${classes.price} ${classes.p2}`}>{`$${totalPrice + 5}`}</span>
+              <span className={`${classes.price} ${classes.p2}`}>{`$${
+                totalPrice + 5
+              }`}</span>
             </span>
             <div className={classes.cn}>
-            <button className={classes.btn}>Proceed to Checkout</button>
+              <button
+                className={classes.btn}
+                onClick={() => {
+                  checkout({
+                    lineItems: [
+                      { price: "price_1OvZ2uSCq7Q0sCcX0LswlYuZ", quantity: 1 },
+                    ],
+                  });
+                }}
+              >
+                Proceed to Checkout
+              </button>
             </div>
-          
+          </div>
         </div>
       </div>
-
-    </div>
       <Footer />
     </>
   );
---
+++ components/Cart/checkout.js
@@ -0,0 +1,18 @@
+import {loadStripe} from "@stripe/stripe-js";
+
+export async function checkout({lineItems}) {
+    let stripePromise =null
+    const getstripe =()=>{
+        if(!stripePromise){
+            stripePromise =loadStripe(process.env.NEXT_PUBLIC_API_KEY)
+        }
+        return stripePromise
+    }
+    const stripe =await getstripe()
+    await stripe.redirectToCheckout({
+        mode:"payment",
+        lineItems,
+        successUrl: `${window.location.origin}?session_id={CHECKOUT_SESSION_ID}`,
+		cancelUrl: window.location.origin
+    })
+}
