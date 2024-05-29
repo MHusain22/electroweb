8cf98b06ff5c7bd9c102883807dbbabdea8b88b2
MHusain22 <husain.vijapura@gmail.com>
2024-05-29 22:04:18 +0530

    rollback

--- next.config.js
+++ next.config.js
@@ -9,7 +9,7 @@
 const nextConfig = {
   reactStrictMode: true,
   env: {
-    NEXTAUTH_URL: "https://electroweb.vercel.app",
+    NEXTAUTH_URL: process.env.NEXTAUTH_URL,
     GOOGLE_CLIENT_ID: process.env.GOOGLE_CLIENT_ID,
     GOOGLE_CLIENT_SECRET: process.env.GOOGLE_CLIENT_SECRET,
   },
