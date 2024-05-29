523461c6bfe9e57c8ab8a7069661e5b797ca393d
MHusain22 <husain.vijapura@gmail.com>
2024-05-29 22:26:04 +0530

    login uri

--- pages/api/auth/[...nextauth].js
+++ pages/api/auth/[...nextauth].js
@@ -12,7 +12,7 @@ export const authOptions = {
     // ...add more providers here
 ],
 pages: {
-  signIn: '/auth/signin',
+  signIn: '/auth/login',
   signOut: '/auth/signout',
   error: '/auth/error', // Error code passed in query string as ?error=
   verifyRequest: '/auth/verify-request', // (used for check email message)
