07cf10638f64d4183af69b580523a0c0c1c0e30f
MHusain22 <husain.vijapura@gmail.com>
2024-05-30 11:48:58 +0530

    secret env added

--- pages/api/auth/[...nextauth].js
+++ pages/api/auth/[...nextauth].js
@@ -9,9 +9,8 @@ export const authOptions = {
         clientId:process.env.GOOGLE_CLIENT_ID,
         clientSecret:process.env.GOOGLE_CLIENT_SECRET,
     }),
-    // ...add more providers here
 ],
+secret: process.env.NEXTAUTH_SECRET,
 }
 
 export default NextAuth(authOptions)
-
